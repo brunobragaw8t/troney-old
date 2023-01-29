fd80dbe8e76aac67ae87e41eca87c535e82acf7e
Bruno Braga <brunobragaw8t@gmail.com>
2023-01-29 17:01:23 +0000

    Create BaseAlert component

---
+++ src/components/BaseAlert.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { computed } from "vue";
+
+export interface IBaseAlert {
+  type: "danger" | "success";
+  message: string;
+}
+
+const props = defineProps<IBaseAlert>();
+
+const typeStyle = computed(() => {
+  switch (props.type) {
+    case "danger":
+      return "border-[#f5c2c7] bg-[#f8d7da] text-[#842029]";
+
+    case "success":
+      return "border-[#badbcc] bg-[#d1e7dd] text-[#0f5132]";
+
+    default:
+      return "";
+  }
+});
+</script>
+
+<template>
+  <div class="rounded border border-solid p-4 text-sm" :class="typeStyle">
+    {{ message }}
+  </div>
+</template>
