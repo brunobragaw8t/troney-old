e49bf1f465d9c1674479ee955d9561dcc2646a1d
Bruno Braga <brunobragaw8t@gmail.com>
2023-01-30 21:48:57 +0000

    Create FormLogin component

---
+++ src/components/FormLogin.vue
@@ -0,0 +1,79 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import BaseForm from "@/components/BaseForm.vue";
+import InputText from "@/components/InputText.vue";
+import BaseButton from "@/components/BaseButton.vue";
+import BaseAlert from "@/components/BaseAlert.vue";
+
+/**
+ * State
+ */
+
+const form = ref({
+  data: {
+    email: "",
+    password: "",
+  },
+  response: {
+    type: "",
+    message: "",
+  },
+  isLoading: false,
+});
+
+/**
+ * Functions
+ */
+
+function signIn(): void {
+  console.log("Logging in...");
+}
+
+/**
+ * Emit
+ */
+
+const emit = defineEmits<{
+  (e: "displayForm", form: string): void;
+}>();
+</script>
+
+<template>
+  <BaseForm @submit.prevent="signIn">
+    <InputText
+      type="email"
+      placeholder="Your email"
+      icon="far fa-envelope"
+      v-model="form.data.email"
+    />
+
+    <InputText
+      type="password"
+      placeholder="Your password"
+      icon="fas fa-key"
+      v-model="form.data.password"
+    />
+
+    <BaseButton
+      tag="button"
+      label="Login"
+      icon="fas fa-sign-in"
+      :is-loading="form.isLoading"
+    />
+
+    <BaseAlert
+      v-if="form.response.type && form.response.message"
+      :type="form.response.type"
+      :message="form.response.message"
+    />
+
+    <BaseButton
+      tag="button"
+      label="Register"
+      icon="fas fa-user-plus"
+      variant="secondary"
+      type="button"
+      @click="emit('displayForm', 'register')"
+    />
+  </BaseForm>
+</template>
