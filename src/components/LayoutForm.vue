9cc21299fa4203628dbb0d6fc7d6e77852a7255e
Bruno Braga <brunobragaw8t@gmail.com>
2023-01-30 18:26:25 +0000

    Create layouts for main and form pages

---
+++ src/components/LayoutForm.vue
@@ -0,0 +1,7 @@
+<template>
+  <div
+    class="flex items-center justify-center min-h-screen px-4 bg-secondary-400"
+  >
+    <slot />
+  </div>
+</template>
---
+++ src/components/LayoutMain.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="flex flex-col gap-12 min-h-screen py-12 px-4 bg-secondary-400">
+    <slot />
+  </div>
+</template>
