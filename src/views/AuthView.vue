a2b3e784c8567358ca6ba2358834ce61e2e9a359
Bruno Braga <brunobragaw8t@gmail.com>
2023-01-30 22:00:17 +0000

    Create AuthView and its route

---
+++ src/views/AuthView.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import LayoutForm from "@/components/LayoutForm.vue";
+import FormLogin from "@/components/FormLogin.vue";
+// import FormRegister from "@/components/FormRegister.vue";
+
+/**
+ * State
+ */
+
+const displayedForm = ref("login");
+
+function displayForm(form: string): void {
+  displayedForm.value = form;
+}
+</script>
+
+<template>
+  <LayoutForm>
+    <FormLogin v-if="'login' === displayedForm" @display-form="displayForm" />
+
+    <!-- <FormRegister -->
+    <!--   v-if="'register' === displayedForm" -->
+    <!--   @display-form="displayForm" -->
+    <!-- /> -->
+  </LayoutForm>
+</template>
