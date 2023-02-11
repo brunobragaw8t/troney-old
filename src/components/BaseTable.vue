4087c18fa8b08134c6d74807a7170d3b9e9c14dc
Bruno Braga <brunobragaw8t@gmail.com>
2023-02-11 10:15:22 +0000

    feature: create BaseTable component

---
+++ src/components/BaseTable.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+export interface IBaseTableColumn {
+  title: string;
+  align: string;
+  field: string;
+}
+
+export interface IBaseTableRow {
+  [x: string]: string | number;
+}
+
+export interface IBaseTable {
+  columns: IBaseTableColumn[];
+  rows: IBaseTableRow[];
+}
+
+defineProps<IBaseTable>();
+</script>
+
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th
+          v-for="(col, i) in columns"
+          :key="i"
+          :class="`px-2 text-white ${col.align}`"
+        >
+          {{ col.title }}
+        </th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <tr v-for="(row, i) in rows" :key="i">
+        <td
+          v-for="(col, j) in columns"
+          :key="j"
+          :class="`px-2 text-white ${col.align}`"
+        >
+          {{ row[col.field] }}
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
